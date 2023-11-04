d0bc5f9a6b6c13453712cddd68ee64513c29b937
Ashish Bagate <83308580+ashish000777@users.noreply.github.com>
2023-11-04 10:11:43 +0530

    Create Captcha.py

---
+++ Captcha.py
@@ -0,0 +1,43 @@
+# Python program to automatically generate CAPTCHA and
+# verify user
+import random
+
+# Returns true if given two strings are same
+def checkCaptcha(captcha, user_captcha):
+	if captcha == user_captcha:
+		return True
+	return False
+
+# Generates a CAPTCHA of given length
+def generateCaptcha(n):
+	
+	# Characters to be included
+	chrs = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	
+	# Generate n characters from above set and
+	# add these characters to captcha.
+	captcha = ""
+	while (n):
+		captcha += chrs[random.randint(1, 1000) % 62]
+		n -= 1
+	return captcha
+
+# Driver code
+
+n = int(input("Enter length of captcha: "))
+
+# Generate a random CAPTCHA
+captcha = generateCaptcha(n)
+print(captcha)
+
+# Ask user to enter a CAPTCHA
+print("Enter above CAPTCHA:")
+usr_captcha = input()
+
+# Notify user about matching status
+if (checkCaptcha(captcha, usr_captcha)):
+	print("CAPTCHA Matched")
+else:
+	print("CAPTCHA Not Matched")
+
+# This code is contributed by shubhamsingh10
